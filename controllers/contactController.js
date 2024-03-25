f554a7bee32ceaff5ad73f07d7ac4f4187f7907a
Arthath Agary <arthathagary@gmail.com>
2024-03-25 12:57:17 +0530

    create CRUD api for contacts

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -1,16 +1,66 @@
+const Contact = require("../models/contactModel");
+
 const getAllContacts = async (req, res, next) => {
-  res.status(200).json({ success: "hello" });
+  const contacts = await Contact.find({});
+  res.status(200).json({
+    success: true,
+    count: contacts.length,
+    contacts,
+  });
 };
 
 const createContact = async (req, res, next) => {
-  res.status(200).json({ success: "hello" });
+  const contact = await Contact.create(req.body);
+  res.status(201).json({
+    success: true,
+    contact,
+  });
 };
 
-const getSingleContact = async (req, res, next) => {};
+const getSingleContact = async (req, res, next) => {
+  const contact = await Contact.findById(req.params.id);
+  if (!contact) {
+    return next(new ErrorHandler("Contact Not Found", 404));
+  }
+  res.status(201).json({
+    success: true,
+    contact,
+  });
+};
 
-const updateContact = async (req, res, next) => {};
+const updateContact = async (req, res, next) => {
+  let contact = await Contact.findById(req.params.id);
+  if (!contact) {
+    return res.status(404).json({
+      success: false,
+      message: "Contact Not Found",
+    });
+  }
+  contact = await Contact.findByIdAndUpdate(req.params.id, req.body, {
+    runValidators: true,
+    new: true,
+  });
+  res.status(201).json({
+    success: true,
+    contact,
+  });
+};
 
-const deleteContact = async (req, res, next) => {};
+const deleteContact = async (req, res, next) => {
+  const contact = await Contact.findById(req.params.id);
+  if (!contact) {
+    return res.status(404).json({
+      success: false,
+      message: "Contact Not Found",
+    });
+  }
+  await contact.deleteOne();
+
+  res.status(200).json({
+    success: true,
+    message: "Contact Deleted",
+  });
+};
 
 module.exports = {
   getAllContacts,
--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const app = require("./app");
+const connectDB = require("./config/database");
+
 const dotenv = require("dotenv");
 const path = require("path");
 
 dotenv.config({ path: path.join(__dirname, "config/config.env") });
+
+connectDB();
 
 app.listen(process.env.PORT, () => {
   console.log(`App listen on ${process.env.PORT}`);
