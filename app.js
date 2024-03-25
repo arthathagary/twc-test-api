2708b4dcc29ad961333028f55c2f796fc73cd683
Arthath Agary <arthathagary@gmail.com>
2024-03-25 22:40:39 +0530

    add cors

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const app = express();
 const cookieParser = require("cookie-parser");
+const cors = require("cors");
 
 const contacts = require("./routes/contact");
 const users = require("./routes/user");
 
 app.use(express.json());
+app.use(cors());
 app.use(cookieParser());
 
 app.use("/api/v1", users);
