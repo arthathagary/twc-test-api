76519976805815b41f54c5f9631ddd32b84af2eb
Arthath Agary <arthathagary@gmail.com>
2024-04-01 02:19:07 +0530

    revert prod

--- app.js
+++ app.js
@@ -7,30 +7,17 @@ const contacts = require("./routes/contact");
 const users = require("./routes/user");
 const authenticateToken = require("./lib/authenticateToken");
 
-const allowedOrigins = [
-  "http://localhost:3000",
-  "https://twc-test-web-three.vercel.app",
-];
-
 app.use(express.json());
-// app.use(cors());
+
 app.use(
   cors({
-    origin: (origin, callback) => {
-      if (!origin) return callback(null, true);
-
-      if (allowedOrigins.indexOf(origin) !== -1) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
+    origin: "http://localhost:3000",
     credentials: true,
   })
 );
 app.use(cookieParser());
 
-app.options("/api/v1/login", cors());
+// app.options("/api/v1/login", cors());
 
 app.use("/api/v1", users);
 app.use("/api/v1", authenticateToken, contacts);
